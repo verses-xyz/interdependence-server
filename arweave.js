e4b96c41743565b1eaf6ce4ed9dd41e0edcf6cb4
Jacky Zhao <j.zhao2k19@gmail.com>
2021-11-01 14:01:15 -0700

    server impl

---
+++ arweave.js
@@ -0,0 +1,98 @@
+require('dotenv').config()
+const Arweave = require('arweave')
+
+const arweave = Arweave.init({
+  host: 'arweave.net',
+  port: 443,
+  protocol: 'https',
+  timeout: 20000,
+  logging: false,
+})
+
+const ADMIN_ADDR = JSON.parse(process.env.ARWEAVE_ADDRESS)
+const KEY = JSON.parse(process.env.ARWEAVE_KEY)
+const DOC_TYPE = "interdependence_doc_type"
+const DOC_ORIGIN = "interdependence_doc_origin"
+const DOC_REF = "interdependence_doc_ref"
+const SIG_NAME = "interdependence_sig_name"
+const SIG_HANDLE = "interdependence_sig_handle"
+const SIG_ADDR = "interdependence_sig_addr"
+const SIG_ISVERIFIED = "interdependence_sig_verified"
+const VERIFICATION_HANDLE = "interdependence_verif_handle"
+const VERIFICATION_ADDR = "interdependence_verif_addr"
+
+export async function checkIfVerified(handle, address) {
+  const req = await fetch('https://arweave.net/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+      query {
+        transactions(
+          tags: [
+            {
+              name: "${DOC_TYPE}",
+              values: ["verification"]
+            }
+          ]
+        ) {
+          edges {
+            node {
+              id
+              owner {
+                address
+              }
+              tags {
+                name
+                value
+              }
+            }
+          }
+        }
+      }
+      `
+    })
+  })
+
+  const json = await req.json()
+  for (const edge of json.data.transactions.edges) {
+    const n = edge.node
+    if (n.owner.address === ADMIN_ADDR) {
+      const parsedHandle = n.tags.find(tag => tag.name === VERIFICATION_HANDLE).value
+      const parsedAddress = n.tags.find(tag => tag.name === VERIFICATION_ADDR).value
+      if (handle === parsedHandle && address === parsedAddress) {
+        return n.id
+      }
+    }
+  }
+  return false
+}
+
+export async function persistVerification(handle, address) {
+  let transaction = await arweave.createTransaction({
+    data: handle
+  }, KEY)
+  transaction.addTag(DOC_TYPE, 'verification')
+  transaction.addTag(VERIFICATION_HANDLE, handle)
+  transaction.addTag(VERIFICATION_ADDR, address)
+  await arweave.transactions.sign(transaction, KEY)
+  return {
+    ...await arweave.transactions.post(transaction),
+    id: transaction.id,
+  }
+}
+
+export async function signDeclaration(declarationId, address, name, handle, isVerified) {
+  let transaction = await arweave.createTransaction({ data: handle }, KEY)
+  transaction.addTag(DOC_TYPE, 'signature')
+  transaction.addTag(DOC_REF, declarationId)
+  transaction.addTag(SIG_NAME, name)
+  transaction.addTag(SIG_HANDLE, handle)
+  transaction.addTag(SIG_ADDR, address)
+  transaction.addTag(SIG_ISVERIFIED, isVerified)
+  await arweave.transactions.sign(transaction, KEY)
+  return await arweave.transactions.post(transaction)
+}
---
+++ index.js
@@ -0,0 +1,100 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const port = 8080
+const TWEET_TEMPLATE = "Verifying my identity to sign the Declaration of Interdependence: "
+const Twitter = require('twitter')
+const {checkIfVerified, persistVerification, signDeclaration} = require("./arweave")
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  bearer_token: process.env.BEARER_TOKEN
+})
+
+app.get('/healthz', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/check/:handle/:address', (req, res) => {
+  const { handle, address } = req.params
+  checkIfVerified(handle, address).then(result => {
+    if (result) {
+      res.json({ verified: true, tx: result })
+    } else {
+      res.json({ verified: false })
+    }
+  })
+})
+
+// post: include name, address (from MM), handle
+app.post('/sign/:declaration', (req, res) => {
+  const declarationId = req.params.declaration
+  const {
+    name,
+    address,
+    handle,
+  } = req.body
+
+  // did the user include a handle?
+  if (handle) {
+    // verify if user is signed
+    checkIfVerified(handle, address).then(result => {
+      const verified = !!result
+      signDeclaration(declarationId, address, name, handle, verified)
+        .then(res.json)
+        .catch(e => res.status(500).send(e))
+    })
+  } else {
+    // pure metamask sig
+    signDeclaration(declarationId, address, name, '', false)
+      .then(res.json)
+      .catch(e => res.status(500).send(e))
+  }
+})
+
+// post: include address (from MM)
+app.post('/verify/:handle', (req, res) => {
+  const handle = req.params.handle
+  const {
+    address,
+  } = req.body
+
+  client.get('statuses/user_timeline', {
+    screen_name: handle,
+    include_rts: false,
+    count: 5,
+  }, (error, tweets, response) => {
+    if (!error) {
+      for (const tweet of tweets) {
+        const parsedAddress = tweet.text.slice(TWEET_TEMPLATE.length)
+        if (tweet.text.startsWith(TWEET_TEMPLATE) && (parsedAddress === address)) {
+          // check to see if already linked
+          checkIfVerified(handle, address)
+            .then(result => {
+              if (result) {
+                // already linked
+                res.json({ tx: result })
+              } else {
+                // need to link
+                persistVerification(handle, address)
+                  .then((tx) => res.status(201).json(tx))
+                  .catch(e => res.status(500).send(e))
+              }
+            })
+          return
+        }
+      }
+      res.status(404).send('no matching tweets found')
+    } else {
+      res.status(400).send(error)
+    }
+  })
+})
+
+app.listen(port, () => {
+  console.log(`server started on :${port}`)
+})
